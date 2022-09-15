025fcf22b7d3338df942dc4583df0a9e9b885871
Jubatus Project <63377116+jubatusproject@users.noreply.github.com>
2022-09-14 22:20:01 -0500

    updates to see deployment changes without re-build images, adding nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi there from WebRoot</h2>");
+    res.send("<h1>Hi there from root folder!!!</h1>");
 });
 
 app.get("/app", (req, res) => {
