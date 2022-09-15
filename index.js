f6c8007a7dce1728e1de8bea3f4e0a673e038b42
Jubatus Project <63377116+jubatusproject@users.noreply.github.com>
2022-09-14 20:50:22 -0500

    new entry point

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi there</h2>");
+    res.send("<h2>Hi there from WebRoot</h2>");
+});
+
+app.get("/app", (req, res) => {
+    res.send("<h2>Hi there from /app folder</h2>");
 });
 
 const port = process.env.port || 3000;
