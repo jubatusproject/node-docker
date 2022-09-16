7a5cdc4c696414a8ef30bde48ef535af856e3d41
Jubatus Project <63377116+jubatusproject@users.noreply.github.com>
2022-09-16 18:31:48 -0500

    reporting used port on web page

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-    res.send("<h1>Hi there from root folder!!!</h1>");
+    res.send(`<h1>Hi there from / folder (PORT=${port})</h1>`);
 });
 
 app.get("/app", (req, res) => {
-    res.send("<h2>Hi there from /app folder!!!</h2>");
+    res.send(`<h2>Hi there from /app folder (PORT=${port})</h2>`);
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
