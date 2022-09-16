19b7e207ca3ea5101e3237426023135837e075e6
Jubatus Project <63377116+jubatusproject@users.noreply.github.com>
2022-09-16 11:49:15 -0500

    Implementing correctly Environment variables for PORT

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/app", (req, res) => {
-    res.send("<h2>Hi there from /app folder</h2>");
+    res.send("<h2>Hi there from /app folder!!!</h2>");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
